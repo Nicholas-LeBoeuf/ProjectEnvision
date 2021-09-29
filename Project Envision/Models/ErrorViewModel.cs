a540c42d786ee155e07ca43d8a7a86ca37aee986
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-09-29 00:27:44 -0400

    Add project files.

---
+++ Project Envision/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Project_Envision.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
