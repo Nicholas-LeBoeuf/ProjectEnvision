fd7529883f40f5995808d90cfea23cd56578121d
Tyler Scott <tyler.scott3@snhu.edu>
2021-10-20 21:51:04 -0400

    10/20/2021 Update Create Board backend

--- Project Envision/Controllers/MainWindowController.cs
+++ Project Envision/Controllers/MainWindowController.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MySql.Data.MySqlClient;
+using Project_Envision.Models;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
-using Project_Envision.Models;
-using MySql.Data.MySqlClient;
-using System.Data;
-using Microsoft.Extensions.Logging;
-using System.Diagnostics;
 
 namespace Project_Envision.Controllers
 {
@@ -15,7 +13,7 @@ namespace Project_Envision.Controllers
     {
         public IActionResult Index()
         {
-            return View("ChooseBoard");
+            return View();
         }
 
         public IActionResult Main()
@@ -25,9 +23,72 @@ namespace Project_Envision.Controllers
 
         public IActionResult CreateBoard(CreateboardModel Cb)
         {
-                return View();
-        }
-        public IActionResult ChooseBoard()
+
+            if (ModelState.IsValid)
+            {
+
+                MySqlConnection conn = new MySqlConnection(Database_connection.m_connection);
+
+                conn.Open();
+
+                string counttxt = $"SELECT * FROM createboard Where user_id = '" + ModelItems.m_userid + "'";
+                MySqlCommand countcmd = new MySqlCommand(counttxt, conn);
+                
+                MySqlDataReader tRead;
+
+                int count = 0;
+
+                using (tRead = countcmd.ExecuteReader())
+                {
+                    if (tRead.HasRows)
+                    {
+
+                        while (tRead.Read())
+                        {
+                            count++;
+                        }
+                    }
+                }
+                if (count >= 5)
+                {
+                    ViewBag.message = "You Have Max Boards";
+                    tRead.Close();
+                    conn.Close();
+                    return View("CreateBoard");
+                }
+                string txtcmd = $"SELECT* FROM createboard where board_name = '" + Cb.board_name + "' AND user_id = '" + ModelItems.m_userid + "'";
+                MySqlCommand textcmd = new MySqlCommand(txtcmd, conn);
+                
+
+                    using (tRead = textcmd.ExecuteReader())
+                    {
+                        if (tRead.Read())
+                        {
+                            ViewBag.message = "You already have board with that name";
+                            tRead.Close();
+                            conn.Close();
+                            return View("CreateBoard");
+                        }
+                    }
+
+                    string txtcmd2 = $"Insert into createboard (board_name,user_id,username, board_description)" + $"values ( @board_name,@user_id,@username, @board_description) ";
+                    MySqlCommand cmd3 = new MySqlCommand(txtcmd2, conn);
+                    cmd3.CommandType = CommandType.Text;
+                    cmd3.Parameters.AddWithValue("@board_name", Cb.board_name);
+                    cmd3.Parameters.AddWithValue("@board_description", Cb.board_Description);
+                    cmd3.Parameters.AddWithValue("@user_id", ModelItems.m_userid);
+                    cmd3.Parameters.AddWithValue("@username", ModelItems.m_username);
+
+                    cmd3.Prepare();
+                    cmd3.ExecuteReader();
+                    conn.Close();
+                    return RedirectToAction("ChooseBoard");
+            }
+            
+            return View("CreateBoard");
+            }
+
+            public IActionResult ChooseBoard()
         {
             return View();
         }
--- Project Envision/Models/CreateboardModel.cs
+++ Project Envision/Models/CreateboardModel.cs
@@ -2,13 +2,10 @@ using System.ComponentModel.DataAnnotations;
 namespace Project_Envision.Models
 {
     public class CreateboardModel
-    {    
-        public int user_id  { get; set; }
-
-        public int sprint_id { get; set; }
-
+    {
+        [Required(ErrorMessage = "Required field. *")]
         public string board_name { get; set; }
 
-        public string username { get; set; }
+        public string board_Description { get; set; }
     }
 }
