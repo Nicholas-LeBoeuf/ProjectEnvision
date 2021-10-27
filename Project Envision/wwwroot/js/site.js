98835fe8e05b41459970ddd42efb57232d2efa7d
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-10-26 21:49:05 -0400

    Created NAV bar and styled it completely. just have not linked any buttons besides logout (need to make views  for those).

--- Project Envision/wwwroot/js/site.js
+++ Project Envision/wwwroot/js/site.js
@@ -1,4 +1,9 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function openNav()
+{
+    document.getElementById("mySidebar").style.width = "250px";
+}
 
-// Write your JavaScript code.
+function closeNav()
+{
+    document.getElementById("mySidebar").style.width = "0";
+}
