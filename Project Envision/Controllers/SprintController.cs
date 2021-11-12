17049c264294306ec63ea9ca57ca01f31ae5a44a
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-11-12 11:05:10 -0500

    Created the sprint view as well as the controller and folders that correspond to it. SprintController returns the correct view. Also added the sprint page to the nav bar

---
+++ Project Envision/Controllers/SprintController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Envision.Controllers
+{
+    public class SprintController : Controller
+    {
+        public IActionResult Sprint()
+        {
+            return View();
+        }
+    }
+}
