35aea43ac6220a9410a79a2930ff88c50b0a993b
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-11-15 15:00:03 -0500

    fixed forgot password 2 buttons

--- Project Envision/Controllers/HomeController.cs
+++ Project Envision/Controllers/HomeController.cs
@@ -215,10 +215,8 @@ namespace Project_Envision.Controllers
             return View();
         }
 
-        public IActionResult ForgotPassword2(ForgotPassword2 forgotPassword2)
+     /*   public IActionResult ForgotPassword2(ForgotPassword2 forgotPassword2)
         {
-
-
             if (forgotPassword2.SecurityCode == ModelItems.code)
             {
                 return View("ForgotPassword3");
@@ -228,6 +226,11 @@ namespace Project_Envision.Controllers
                 ViewBag.message = "Incorrect Code";
                 return View();
             }
+        }*/
+
+        public IActionResult ForgotPassword2()
+        {
+            return View("ForgotPassword2");
         }
 
         public IActionResult ForgotPassword3(ForgotPassword3 forgotPassword3)
--- Project Envision/Models/Setup/Database connection.cs
+++ Project Envision/Models/Setup/Database connection.cs
@@ -7,6 +7,6 @@ namespace Project_Envision.Models
 {
     public class Database_connection
     {
-        public static string m_Connection = "server=localhost; Username= Guess; Password = welcome123!; persistsecurityinfo=True;database= ProjectEnvision";
+        public static string m_Connection = "server=localhost; Username= root; Password = password; persistsecurityinfo=True;database= project";
     }
 }
