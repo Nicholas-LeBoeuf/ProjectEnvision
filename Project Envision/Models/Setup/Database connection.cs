920f3212d758530f0476d1d1c5e6fda4a50e9022
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2021-11-20 18:13:00 -0500

    Transferred add/remove teammate from a modal pop up to a view

--- Project Envision/Models/Setup/Database connection.cs
+++ Project Envision/Models/Setup/Database connection.cs
@@ -7,7 +7,7 @@ namespace Project_Envision.Models
 {
     public class Database_connection
     {
-        public static string m_Connection = "server=localhost; Username= Guess; Password = welcome123!; persistsecurityinfo=True;database= projectEnvision";
-        // public static string m_Connection = "server=localhost; Username= root; Password = password; persistsecurityinfo=True;database= project";
+        // public static string m_Connection = "server=localhost; Username= Guess; Password = welcome123!; persistsecurityinfo=True;database= projectEnvision";
+        public static string m_Connection = "server=localhost; Username= root; Password = password; persistsecurityinfo=True;database= project";
     }
 }
