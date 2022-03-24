47fb9919079e9ae39b4fef9c1ec301649b87e1b0
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2022-03-24 11:44:01 -0400

    Added burndown chart to the view as well as the sample data

--- Project Envision/Models/BurndownChart/BurndownInputs.cs
+++ Project Envision/Models/BurndownChart/BurndownInputs.cs
@@ -1,6 +1,41 @@
-﻿namespace Project_Envision.Models.BurndownChart
+﻿using System.Collections.Generic;
+
+namespace Project_Envision.Models
 {
-    public class BurndownInputs
+    public partial class SampleData
     {
+        public static readonly IEnumerable<Burndown> BurndownList = new[] {
+            new Burndown { Day= 1, Temperature= 11 },
+            new Burndown { Day= 2, Temperature= 7 },
+            new Burndown { Day= 3, Temperature= 6 },
+            new Burndown { Day= 4, Temperature= 8 },
+            new Burndown { Day= 5, Temperature= 7 },
+            new Burndown { Day= 6, Temperature= 7 },
+            new Burndown { Day= 7, Temperature= 11 },
+            new Burndown { Day= 8, Temperature= 9 },
+            new Burndown { Day= 9, Temperature= 5 },
+            new Burndown { Day= 10, Temperature= 8 },
+            new Burndown { Day= 11, Temperature= 6 },
+            new Burndown { Day= 12, Temperature= 9 },
+            new Burndown { Day= 13, Temperature= 8 },
+            new Burndown { Day= 14, Temperature= 6 },
+            new Burndown { Day= 15, Temperature= 6 },
+            new Burndown { Day= 16, Temperature= 6 },
+            new Burndown { Day= 17, Temperature= 10 },
+            new Burndown { Day= 18, Temperature= 9 },
+            new Burndown { Day= 19, Temperature= 12 },
+            new Burndown { Day= 20, Temperature= 9 },
+            new Burndown { Day= 21, Temperature= 8 },
+            new Burndown { Day= 22, Temperature= 13 },
+            new Burndown { Day= 23, Temperature= 9 },
+            new Burndown { Day= 24, Temperature= 7 },
+            new Burndown { Day= 25, Temperature= 6 },
+            new Burndown { Day= 26, Temperature= 11 },
+            new Burndown { Day= 27, Temperature= 8 },
+            new Burndown { Day= 28, Temperature= 7 },
+            new Burndown { Day= 29, Temperature= 9 },
+            new Burndown { Day= 30, Temperature= 7 },
+            new Burndown { Day= 31, Temperature= 3 }
+        };
     }
 }
