689ee23afc67c52ad0485af46330825c4ed53524
Nicholas LeBoeuf <nicholas.leboeuf@snhu.edu>
2022-03-31 14:02:38 -0400

    Fixed change username and password

--- Project Envision/Models/Settings/UserSettings.cs
+++ Project Envision/Models/Settings/UserSettings.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Project_Envision.Models.Settings
+namespace Project_Envision.Models
 {
     public class UserSettings
     {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string ConfirmPassword { get; set; }
     }
 }
---
+++ Project Envision/Models/Settings/UserSettingsItems.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_Envision.Models
+{
+    public class UserSettingsItems
+    {
+        public static string m_Username;
+        public static string m_Password;
+        public static string m_ConfirmPassword;
+
+        public string Username
+        {
+            get => m_Username;
+            set => m_Username = value;
+        }
+
+        public string Password
+        {
+            get => m_Username;
+            set => m_Username = value;
+        }
+
+        public string ConfirmPassword
+        {
+            get => m_ConfirmPassword;
+            set => m_ConfirmPassword = value;
+        }
+
+    }
+}
